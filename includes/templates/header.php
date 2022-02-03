ced6519e5d410c45e5db2cb714465c85f290b539
charlie-uy <caeruy@gmail.com>
2022-02-03 16:13:07 -0300

    299 - Archivos para CRUD

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,26 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contacto</title>
-    <link rel="stylesheet" href="build/css/app.css"
+    <link rel="stylesheet" href="/build/css/app.css"
 </head>
 <body>
     <header class="header <?php echo $inicio ? 'inicio' : ''; ?>">
         <div class="contenedor contenido-header">
             <div class="barra">
-                <a href="index.php">
-                    <img src="build/img/logo.svg" alt="Logo de Bienes Raices"/>
+                <a href="/index.php">
+                    <img src="/build/img/logo.svg" alt="Logo de Bienes Raices"/>
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="Icono menu responsive">
+                    <img src="/build/img/barras.svg" alt="Icono menu responsive">
                 </div>
                 <div class="derecha">
-                    <img src="build/img/dark-mode.svg" alt="botón modo oscuro" class="dark-mode-boton">
+                    <img src="/build/img/dark-mode.svg" alt="botón modo oscuro" class="dark-mode-boton">
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/nosotros.php">Nosotros</a>
+                        <a href="/anuncios.php">Anuncios</a>
+                        <a href="/blog.php">Blog</a>
+                        <a href="/contacto.php">Contacto</a>
                     </nav>
                 </div>
             </div> <!-- .barra -->
