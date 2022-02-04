3b18edaa89626e52ece2ff7d3ef587cee1cf7a43
charlie-uy <caeruy@gmail.com>
2022-02-04 15:33:07 -0300

    303 - Superglobales GET y POST
    Actualización package.json

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -5,6 +5,33 @@
     
     $db = conectarDB();
 
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // echo "<pre>";
+        // var_dump($_POST);
+        // echo "</pre>";
+
+        $titulo = $_POST['titulo'];
+        $precio = $_POST['precio'];
+        $descripcion = $_POST['descripcion'];
+        $habitaciones = $_POST['habitaciones'];
+        $wc = $_POST['wc'];
+        $estacionamiento = $_POST['estacionamiento'];
+        $vendedorId = $_POST['vendedor'];
+
+        // Insertar en la base de datos
+        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorID)
+        VALUES ) ( '$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId'); ";
+
+        // echo $query;
+
+        $resultado = mysqli_query($db, $query);
+
+        if($resultado) {
+            echo "Insertado correctamente";
+        }
+        
+    }
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -12,41 +39,41 @@
     <main class="contenedor seccion">
         <h1>Crear</h1>
 
-        <form class="formulario">
+        <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
             <fieldset>
                 <legend>Información general</legend>
 
                 <label for="titulo">Titulo:</label>
-                <input type="text" id="titulo" placeholder="Título propiedad">
+                <input type="text" id="titulo" name="titulo" placeholder="Título propiedad">
 
-                <label for="imagen">Imagen:</label>
-                <input type="number" id="precio" placeholder="Precio propiedad">
+                <label for="precio">Precio de venta:</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio propiedad">
                 
-                <label for="Imagen">Imagen:</label>
+                <label for="imagen">Imagen:</label>
                 <input type="file" id="imagen" accept="image/jpeg, image/png">
 
                 <label for="descripcion">Descripción</label>
-                <textarea id="descripcion"></textarea>
+                <textarea id="descripcion" name="descripcion"></textarea>
             </fieldset>
 
             <fieldset>
                 <legend>Información de la propiedad</legend>
 
                 <label for="habitaciones">Habitaciones:</label>
-                <input type="number" id="habitaciones" placeholder="Cantidad de habitaciones" min="1" max="9">
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Cantidad de habitaciones" min="1" max="9">
 
                 <label for="wc">Baños:</label>
-                <input type="number" id="wc" placeholder="Cantidad de baños" min="1" max="9">
+                <input type="number" id="wc" name="wc" placeholder="Cantidad de baños" min="1" max="9">
 
                 <label for="estacionamiento">Estacionamientos:</label>
-                <input type="number" id="estacionamiento" placeholder="Cantidad de estacionamientos" min="1" max="9">
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Cantidad de estacionamientos" min="1" max="9">
 
             </fieldset>
 
             <fieldset>
                 <legend>Vendedor</legend>
 
-                <select>
+                <select name="vendedor">
                     <option value="1">Juan</option>
                     <option value="2">Karen</option>
                 </select>
