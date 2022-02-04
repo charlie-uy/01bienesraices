eb528de844fd53d03c4b494925c1fb831a56a2fc
charlie-uy <caeruy@gmail.com>
2022-02-04 14:04:04 -0300

    301 - Conectando la BD

---
+++ includes/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+function conectarDB() : mysqli{
+    $db = mysqli_connect('localhost', 'root', 'root', 'bienes_raices');
+
+    if(!$db) {
+        echo "Error, no se puedo conectar";
+        exit;
+    }
+
+    return $db;
+}
