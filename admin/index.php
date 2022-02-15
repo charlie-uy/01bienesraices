ca5b9bab7b66b53c8451413f418dd335c5eff5fd
charlie-uy <caeruy@gmail.com>
2022-02-15 14:16:21 -0300

    313 - Tabla visualización propiedades

--- admin/index.php
+++ admin/index.php
@@ -7,12 +7,35 @@
 
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
-        <?php if( intval ($resultado) === 1): ?>
+        <?php if( isset ($resultado)): ?>
             <p class="alerta exito">Anuncio creado correctamente</p>
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
-        
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="/imagenes/749174e34a2a9ac8129dfe14d692849b.jpg" class="imagen-tabla"></td>
+                    <td>2500000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-verde-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 
