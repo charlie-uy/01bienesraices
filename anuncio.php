ecec243629ac42e0ce38b2ff0c9fe00763215b66
charlie-uy <caeruy@gmail.com>
2022-02-24 16:38:41 -0300

    321 - Página de anuncio - cierre previo usuario

--- anuncio.php
+++ anuncio.php
@@ -1,40 +1,63 @@
 <?php 
 
+    // ! Validar la URL y que se pase un id válido
+
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    // ¡ Si el id no es válido se redirecciona al admin
+    if (!$id) {
+        header('Location: /');
+    }
+
+    // Base de datos
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+    // ¡ Obtener los datos de la propiedad
+    $consulta = "SELECT * FROM propiedades WHERE id = ${id}";
+    $resultado = mysqli_query($db, $consulta);
+
+    if(!$resultado->num_rows) {
+        header('Location: /');
+    }
+    $propiedad = mysqli_fetch_assoc($resultado);
+
+    // ¡ Cabecera de la página
     require 'includes/funciones.php';
     incluirTemplate('header');
 
 ?>
 
     <main class="contenedor seccion contenido-centrado">
-        <h1>Casa en venta frente al bosque</h1>
+        <h1><?php echo $propiedad['titulo']; ?></h1>
 
-        <picture>
-            <source srcset="build/img/destacada.webp" type="image/webp">
-            <source srcset="build/img/destacada.avif" type="image/avif">
-            <img loading="lazy" src="build/img/destacada.jpg" alt="imagen de la propiedad">
-        </picture>
+        <img src="imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imágen de la propiedad">
 
         <div class="resumen-propiedad">
-            <p class="precio">$ 3.000.000</p>
+            <p class="precio">$ <?php echo $propiedad['precio']; ?></p>
             <ul class="iconos-caracteristicas">
                 <li>
                     <img class="icono" src="build/img/icono_wc.svg" alt="Baños" loading="lazy">
-                    <p>3</p>
+                    <p><?php echo $propiedad['wc']; ?></p>
                 </li>
                 <li>
                     <img class="icono" src="build/img/icono_estacionamiento.svg" alt="Estacionamientos" loading="lazy">
-                    <p>3</p>
+                    <p><?php echo $propiedad['estacionamiento']; ?></p>
                 </li>
                 <li>
                     <img class="icono" src="build/img/icono_dormitorio.svg" alt="Habitaciones" loading="lazy">
-                    <p>4</p>
+                    <p><?php echo $propiedad['habitaciones']; ?></p>
                 </li>
             </ul>
 
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum voluptate labore officiis temporibus veniam fuga, vel, esse et accusantium sit iste quibusdam eaque non architecto quasi necessitatibus. Molestias, modi rem.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum sunt reprehenderit doloremque voluptate delectus amet, a inventore odit, placeat consequuntur dolor minima corporis rerum! Vero sapiente id consequatur animi officiis!</p>
+            <p><?php echo $propiedad['descripcion']; ?></p>
         </div>
 
     </main>
 
-<?php incluirTemplate('footer'); ?>
+<?php
+    // ¡ Cerrar la conexión
+    mysqli_close($db);
+
+    incluirTemplate('footer'); ?>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contacto</title>
+    <title>Bienes Raices</title>
     <link rel="stylesheet" href="/build/css/app.css"
 </head>
 <body>
@@ -28,5 +28,11 @@
                     </nav>
                 </div>
             </div> <!-- .barra -->
+            <?php
+                if($inicio) {
+                    echo "<h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>";
+                }
+            ?>
+
         </div>
     </header>
